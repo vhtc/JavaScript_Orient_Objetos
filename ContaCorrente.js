9f3daca303d311bee58cc36c9e0c782a411e2ab7
vhtc <victor.tuy@hotmail.com>
2024-10-08 13:22:37 -0300

    first commit

---
+++ ContaCorrente.js
@@ -0,0 +1,32 @@
+export class ContaCorrente {
+    agencia;
+    _cliente;
+
+    set cliente(novoValor) {
+        this._cliente = novoValor
+    }
+    
+    //#saldo = 0
+    _saldo = 0;
+
+    sacar(valor) {
+
+        if (this._saldo >= valor) {
+            this._saldo -= valor
+            return valor
+        }
+    }
+
+    depositar(valor) {
+        if (valor <= 0) {
+            return
+        }
+        this._saldo += valor
+    }
+
+    transferir(valor, conta) {
+        const valorSacado = this.sacar(valor)
+        conta.depositar(valorSacado)
+        conta.cidade = "Salvador"
+    }
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+Para instalar o package.json no projeto é necessário o "npm init"
+cadastrar o type module para utilizar os exports imports no javascript
